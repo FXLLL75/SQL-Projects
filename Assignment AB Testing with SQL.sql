f2a3e22b555bf761593e693d826c3a4123ccfa36
FXLLL75 <99883539+FXLLL75@users.noreply.github.com>
2022-09-20 18:58:51 +0200

    AB Testing with SQL & Mode Analytics
    Build a simple testing framework to touch on AB Testing

---
+++ Assignment AB Testing with SQL.sql
@@ -0,0 +1,109 @@
+--We are running an experiment at an item-level, which means all users who visit will see the same page, but the layout of different item pages may differ.
+--Compare this table to the assignment events we captured for user_level_testing.
+--Q1) Does this table have everything you need to compute metrics like 30-day view-binary?
+
+SELECT 
+  * 
+FROM 
+  dsv1069.final_assignments_qa;
+  
+SELECT * FROM dsv1069.final_assignments;
+  
+--ANSWER Q1 : No, we need the date and time of assignment in order to compute some of our metrics like 30-day view-binary.
+
+
+
+
+/*Q2) Write a query and table creation statement to make final_assignments_qa look like the final_assignments table. If you discovered something missing in part 1, you may fill in the value with a place holder of the appropriate data type. */
+
+WITH final_assignments_qa_fixed
+AS 
+(SELECT item_id,
+       test_a AS test_assignment,
+       'test_a' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION ALL
+SELECT item_id,
+       test_b AS test_assignment,
+       'test_b' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION ALL
+SELECT item_id,
+       test_c AS test_assignment,
+       'test_c' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION ALL
+SELECT item_id,
+       test_d AS test_assignment,
+       'test_d' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION ALL
+SELECT item_id,
+       test_e AS test_assignment,
+       'test_e' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa
+UNION ALL
+SELECT item_id,
+       test_f AS test_assignment,
+       'test_f' AS test_number,
+       CAST('2013-01-05 00:00:00' AS timestamp) AS test_start_date
+FROM dsv1069.final_assignments_qa)
+SELECT * FROM final_assignments_qa_fixed
+
+
+
+/*Q3) Use the final_assignments table to calculate the order binary for the 30 day window after the test assignment for item_test_2 (You may include the day the test started)*/
+SELECT order_binary.test_assignment,
+       COUNT(DISTINCT order_binary.item_id) AS num_orders,
+       SUM(order_binary.order_binary_30d) AS sum_orders_bin_30d
+FROM
+  (SELECT assignments.item_id,
+          assignments.test_assignment,
+          MAX(CASE
+                  WHEN (orders.created_at > assignments.test_start_date
+                        AND DATE_PART('day', created_at - test_start_date) <= 30) THEN 1
+                  ELSE 0
+              END) AS order_binary_30d
+   FROM dsv1069.final_assignments AS assignments
+   LEFT JOIN dsv1069.orders AS orders
+     ON assignments.item_id = orders.item_id
+   WHERE assignments.test_number = 'item_test_2'
+   GROUP BY assignments.item_id,
+            assignments.test_assignment) AS order_binary
+GROUP BY order_binary.test_assignment
+
+
+
+/*Q4) Use the final_assignments table to calculate the view binary and average views for the 30 day window, after the test assignment for item_test_2. (You may include the day the test started)*/
+SELECT view_binary.test_assignment,
+       COUNT(DISTINCT view_binary.item_id) AS num_views,
+       SUM(view_binary.view_bin_30d) AS sum_view_bin_30d,
+       AVG(view_binary.view_bin_30d) AS avg_view_bin_30d
+FROM
+  (SELECT assignments.item_id,
+          assignments.test_assignment,
+          MAX(CASE
+                  WHEN (views.event_time > assignments.test_start_date
+                        AND DATE_PART('day', event_time - test_start_date) <= 30) THEN 1
+                  ELSE 0
+              END) AS view_bin_30d
+   FROM dsv1069.final_assignments AS assignments
+   LEFT JOIN dsv1069.view_item_events AS views
+     ON assignments.item_id=views.item_id
+   WHERE assignments.test_number='item_test_2'
+   GROUP BY assignments.item_id,
+            assignments.test_assignment
+   ORDER BY item_id) AS view_binary
+GROUP BY view_binary.test_assignment
+
+
+/* Q5) Use the https://thumbtack.github.io/abba/demo/abba.html to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence.*/
+
+--For orders_bin: lift is -14% – 12% (-1%) and pval is 0.88
+--For view_bin:   lift is -1.6% – 6.1% (2.3%) and pval is 0.25
+-- For test_2, there was no signficant variation of the number of views and orders between control and treatment.
